0a5d4e4c34839f2bb7b64d5e684c2318851a51aa
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-25 21:10:01 -0300

    Enzo3

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação utilizada principalmente para desenvolvimento web, permitindo criar páginas interativas e dinâmicas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'Um loop é uma estrutura que repete um bloco de código enquanto uma condição é verdadeira.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz obrigado em inglês?',
+    'Obrigado em inglês é "Thank you".'
+)
